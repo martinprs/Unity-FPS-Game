4b1933bb6002106b29f91a734abfa8303a738d28
Martin Pruus <martinprs@users.noreply.github.com>
2026-09-18 18:24:17 +0300

    Add Player and camera look & movement scripts

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public float sensitivity = 200f;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float sprintSpeed = 9f;
+    public float crouchSpeed = 2.5f;
+
+    public float jumpHeight = 1.5f;
+    public float gravity = -9.81f;
+
+    public float standingHeight = 1.8f;
+    public float crouchingHeight = 1.0f;
+    public float heightChangeSpeed = 6f;
+
+    CharacterController cc;
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Start()
+    {
+        cc = gameObject.AddComponent<CharacterController>();
+        cc.height = standingHeight;
+    }
+
+    void Update()
+    {
+        isGrounded = cc.isGrounded;
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        float currentSpeed = walkSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl))
+            currentSpeed = sprintSpeed;
+
+        bool crouching = Input.GetKey(KeyCode.LeftControl);
+        if (crouching)
+            currentSpeed = crouchSpeed;
+
+        cc.Move(move * currentSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded && !crouching)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        cc.Move(velocity * Time.deltaTime);
+
+        float targetHeight = crouching ? crouchingHeight : standingHeight;
+        cc.height = Mathf.Lerp(cc.height, targetHeight, Time.deltaTime * heightChangeSpeed);
+    }
+}
